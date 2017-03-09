7136c6c0776e25ce8d96d62e0d5c27cefed047a0
Alec Waters <alech2os@uw.edu>
2017-03-08 22:42:31 -0800

    Add an intro tab and its summary of the data

--- app.r
+++ app.r
@@ -3,8 +3,7 @@
 library("ggplot2")
 library("shiny")
 library("dplyr")
-library("tidyr")
-library("plotly")
+
 #Creates dataframe containing all of the speed dating data, uncleaned
 data <- read.csv("data/Speed Dating Data.csv", stringsAsFactors = FALSE)
 
@@ -21,41 +20,15 @@ measure.up[,5:14]<- measure.up[,5:14]*10
 others.perc <- select(data, iid, gender, age, race, attr5_1,	sinc5_1,	intel5_1,	fun5_1,	amb5_1, attr5_2,	sinc5_2,	intel5_2,	fun5_2,	amb5_2)
 others.perc <- na.omit(others.perc[!duplicated(others.perc),])
 others.perc[,5:14] <- others.perc[,5:14]*10
-#date.anly <- select(data, match,dec,	attr,	sinc,	intel,	fun,	amb,	shar,	like,	prob,	met,	match_es)
-#postdate.data <- select(data, iid,you_call, them_cal, date_3,	numdat_3,	num_in_3)
-#postdate.data <- postdate.data[!duplicated(postdate.data),]
 age.range.opp.sex <- range(look.for.opp.sex$age)
+
 #UI code starts here, will be used by everybody so expect merge conflicts and keep it clean------------
 dating.ui <- fluidPage(
   #master UI code starts here------------------  
-  
-<<<<<<< HEAD
-  sidebarLayout(
-    
-    #We'll use this as the control panel for user control
-    sidebarPanel(
-      selectInput('gen', 'Gender', c("Male" = 1, "Female" = 0, "Both")),
-      sliderInput('agran', 'Age Range', min = age.range.opp.sex[1], max = age.range.opp.sex[2], value = age.range.opp.sex),
-      checkboxGroupInput('rce', 'Race', choices = c("Black/African American" = 1, "Caucasian" = 2, "Hispanic" = 3, "Asian/Pacific Islander" = 4, "Other" = 6), selected = c(1,2,3,4,6) ),
-      selectInput('diffset', 'Different Survey Questions', c("What do you look for in a person?" = "look.for.opp.sex", "What does your same gender look for?" = "same.gen.look.for", "What does the opposite gender look for?" = "opp.gen.look.for", "How do you meausre up to expectations?" = "measure.up", "How you think other percieve you?" = "others.perc")),
-      selectInput('trait', 'Trait of Subject', c("Attractive" = "attr","Sincerety" = "sinc",	"Intellegence" = "intel",	"fun" = "fun",	"ambitious" = "amb", "shared interests" = "shar" ) )
-    ),
-    
-    
-    #We'll use this with a tabsetpanel in order to show graphs, reports, figures, etc
-    mainPanel(
-      plotOutput('plot'),
-      #tableOutput('table'),
-      verbatimTextOutput('event')
-    )
-  )
-  
-  #master UI code ends here--------------------
-)
-=======
   #We'll use this as the control panel for user control
   sidebarPanel(
-    selectInput("field.input", label=h3("Select Field"), 
+    h3("Fields/Careers"),
+    selectInput("field.input", "Select Field", 
                 c("Law"=1, "Math"=2, "Social Science, Psychologist"=3, 
                   "Medical Science, Pharmaceuticals, and Bio Tech"=4, "Engineering"=5,
                   "English/Creative Writing/ Journalism"=6, "History/Religion/Philosophy"=7,
@@ -63,15 +36,40 @@ dating.ui <- fluidPage(
                   "Biological Sciences/Chemistry/Physics"=10, "Social Work"=11,
                   "Undergrad/undecided"=12, "Political Science/International Affairs"=13,
                   "Film"=14, "Fine Arts/Arts Administration"=15, "Languages"=16,
-                  "Architecture"=17, "Other"=18))
-                                
-
+                  "Architecture"=17, "Other"=18)),
+    h3("Survey Response Analysis"),
+    selectInput('trait', 'Trait of Subject', c("Attractive" = "attr","Sincerety" = "sinc",	"Intellegence" = "intel",	"fun" = "fun",	"ambitious" = "amb", "shared interests" = "shar" ) ),
+    selectInput('diffset', 'Different Survey Questions', c("What do you look for in a person?" = "look.for.opp.sex", "What does your same gender look for?" = "same.gen.look.for", "What does the opposite gender look for?" = "opp.gen.look.for", "How do you meausre up to expectations?" = "measure.up", "How you think other percieve you?" = "others.perc")),
+    selectInput('gen', 'Gender', c("Male" = 1, "Female" = 0, "Both"), selected = "Both"),
+    sliderInput('agran', 'Age Range', min = age.range.opp.sex[1], max = age.range.opp.sex[2], value = age.range.opp.sex),
+    checkboxGroupInput('rce', 'Race', choices = c("Black/African American" = 1, "Caucasian" = 2, "Hispanic" = 3, "Asian/Pacific Islander" = 4, "Other" = 6), selected = c(1,2,3,4,6) )
   ),
   
   #We'll use this with a tabsetpanel in order to show graphs, reports, figures, etc
   mainPanel(
-    tabsetPanel(
+    tabsetPanel( 
+      tabPanel("Data Overview",
+               h1("A Speed Dating Experiment"),
+               h3("Where Did This Data Come From?"),
+               p("This data was compiled by Ray Fisman and Sheena Lyengar, two Columbia Business School professors. 
+                 The purpose of their data collection was to find evidence for their paper 'Gender Differences in Mate Selection: Evidence From a Speed Dating Experiment'. 
+                 We found this data source through kaggle which offers users a database of public data sets and other resources similar to the one this app is built around."),
+               helpText("Click here to see the data source:", a("Speed Dating Experiment",target = "_blank", href = "https://www.kaggle.com/annavictoria/speed-dating-experiment")),
+               helpText("Click here to see the essay that utalizes this data set:", a("Essay", target = "_blank", href = "http://faculty.chicagobooth.edu/emir.kamenica/documents/genderDifferences.pdf")),
+               h3("How Was This Data Collected?"),
+               p("This data was collected using a series of speed dating events ranging from 2002-2004.
+                 As the night continued participants would record their experience with every person they met that night using a rating system that was focused on the attributes of attractiveness, sincerity, intelligence, fun, ambition, and shared interests.
+                 Not only was data collected about the date, but a questionnaire was used to gather information about peoples occupations, habits, demographics, and beliefs."),
+               h3("How Will We Analyze This Data?"),
+               p("To explore the data we broke it up into three parts. The first part of our analysis will explore how carres and occupations have different effects on an individuals attribute ratings and social life. 
+                 Then we will explore the effect that race played in matching during the event. Finally we broke down the survey data to analyze how the experience of speed dating changed the individuals beliefs about themselves and their peers by analyzing how each participants ratings from the different survey questions changed after the experiment was conducted."),
+               h3("Who Would Be Interested in Our Findings?"),
+               p("The main audience for our analysis is college students, considering that we are presenting our findings to a college class. However, this experiment includes a wide range of ages, so if anyone is looking for dating trends this data can apply to anyone."),
+               h3("Notes About The Data"),
+               p("Some key elements of the data to consider. The biggest restriction about the data set is that no experiments were conducted to find dating trends in the same sex. Because of this our analysis is mainly applicable to heterosexual dating trends. Also a variety of races lacked from the experiment, and the majority of the participants were caucasian. Finally the data is not representitive of the entire population, not only are all races and sexual preferences represented but the data set is too small to apply to the population.")),
+      
       tabPanel("Fields/Careers",
+               h1("Analyzing The Effects of Fields/Careers"),
                plotOutput('plot.rate', brush = "plot_brush"),
                verbatimTextOutput("info.rate"),
                p('This bar graph shows how each individual was rated on a scale of 1 to 10 by their partner,
@@ -115,24 +113,28 @@ dating.ui <- fluidPage(
                  
                  p("Although a large majority of the data followed a similar pattern, there are endless inferences that can be made, and it does seem as though the field 
                   you are in can affect why and how you speed date.")
-             
-               
+    ),
+    tabPanel(
+      "Survey Analysis",
+      h1("Breaking Down Survey Responses"),
+      plotOutput('plotbfafpoint', click = 'clk'),
+      verbatimTextOutput('outp'),
+      tableOutput('plotbfafbar'),
+      textOutput('text.gra')
     )
    )
   )
- )
 )
 
->>>>>>> master
+
 #UI code ends here-------------------------------------------------------------------------------------
 
 
 
 #Server code starts here, will be used by everybody so expect merge conflicts and keep it clean--------
 dating.server <- function(input, output) {
-<<<<<<< HEAD
   #master server code starts here------------------  
-  filtered.data <- reactive({
+  filteredSurvey.data <- reactive({
     
     data <-  get(input$diffset)
     if(input$gen == 0 | input$gen == 1){
@@ -156,7 +158,7 @@ dating.server <- function(input, output) {
     }else if (length(input$rce) == 5){
       data <- data  
     }else{
-      data <- NULL
+      return(NULL)
     }
     if(input$diffset == "look.for.opp.sex"){
       data <- select_(data, "iid","gender","age", "race", paste0(input$trait, "1_1"), paste0(input$trait, "1_2"))
@@ -164,15 +166,16 @@ dating.server <- function(input, output) {
       data <- select_(data, "iid", "gender", "age", "race", paste0(input$trait, "4_1"), paste0(input$trait, "4_2"))
     }else if (input$diffset == "opp.gen.look.for"){
       data <- select_(data, "iid", "gender", "age", "race", paste0(input$trait, "2_1"), paste0(input$trait, "2_2"))
-    }else if (input$diffset == "measure.up"){
+    }else if (input$diffset == "measure.up" & input$trait != "shar"){
       data <- select_(data, "iid", "gender", "age", "race", paste0(input$trait, "3_1"), paste0(input$trait, "3_2"))
-    }else if (input$diffset == "others.perc"){
+    }else if (input$diffset == "others.perc" & input$trait != "shar"){
       data <- select_(data, "iid", "gender", "age", "race", paste0(input$trait, "5_1"), paste0(input$trait, "5_2"))
+    }else{
+      return(NULL)
     }
     return(data)
   })
-=======
-  
+
   # Filters the data by field and partner rating
   filtered.rating <- reactive({
     data.info <- data %>% 
@@ -310,17 +313,55 @@ dating.server <- function(input, output) {
   
   
 #master server code starts here------------------  
->>>>>>> master
 
-  output$plot<-renderPlot({
-    
-    p <- ggplot(data = filtered.data())+
-      geom_point(mapping = aes_(x = filtered.data()[,5], y = filtered.data()[,6], color = filtered.data()[,6]>filtered.data()[,5]),position = "jitter")+
-      geom_smooth(mapping = aes(x = filtered.data()[,5], y = filtered.data()[,6]))+
-      labs(title = "Exploring Participant Preferences Over Time", x = "Initial Response Value (Before Speed Dating)", y = "Final Response Value (After Speed Dating)", color = "Increased Rating After Night")+
+  output$plotbfafpoint<-renderPlot({
+    if(is.null(filteredSurvey.data())){
+      return(NULL)
+    }else{
+    p <- ggplot(data = filteredSurvey.data())+
+      geom_point(mapping = aes_(x = filteredSurvey.data()[,5], y = filteredSurvey.data()[,6], color = filteredSurvey.data()[,6]>filteredSurvey.data()[,5]), alpha = 1/5)+
+      geom_smooth(mapping = aes(x = filteredSurvey.data()[,5], y = filteredSurvey.data()[,6]))+
+      labs(title = "Exploring Participant Attribute Preferences Before and After Speed Dating", x = "Initial Response Value (Before Speed Dating)", y = "Final Response Value (After Speed Dating)", color = "Increased Rating After Night")+
+      theme(plot.title = element_text(hjust = 0.5))+
       scale_color_manual(labels = c("Negitive/No Rating Change", "Positive Rating Change"), values = c("red", "blue"))
     
     return(p)
+    }
+  })
+  sumserv <- reactive({
+    `Average Initial Attribute Rating` <- mean(filteredSurvey.data()[,5])
+    `Average Final Attribute Rating` <- mean(filteredSurvey.data()[,6])
+    data <- data.frame(`Average Initial Attribute Rating`, `Average Final Attribute Rating`)
+    return(data)
+  }) 
+  output$plotbfafbar <- renderTable({
+    return(sumserv())
+  
+})
+  
+  output$outp <- renderText({
+      if(is.null(input$clk)){
+        return("Pick a Point \n")
+      }else{
+        paste0("Initial Rating = ", round(input$clk$x, 1), "\n", "Final Rating = ", round(input$clk$y,1), "\n")
+      }
+    })
+  output$text.gra <- renderText({
+    if(input$diffset == "look.for.opp.sex"){
+      ques <- "What do you look for in a person?"    
+    }else if(input$diffset == "same.gen.look.for"){
+      ques <- "What does your same gender look for?"
+    }else if (input$diffset == "opp.gen.look.for"){
+      ques <- "What does the opposite gender look for?"
+    }else if (input$diffset == "measure.up"){
+      ques <- "How do you meausre up to expectations?"
+    }else if (input$diffset == "others.perc"){
+      ques <- "How you think other percieve you?"
+    }
+    `Average Initial Attribute Rating` <- round(mean(filteredSurvey.data()[,5]),1)
+    `Average Final Attribute Rating` <- round(mean(filteredSurvey.data()[,6]),1)
+    text.out <- paste("When asked the question'",ques,"' the average initial response about the trait was", `Average Initial Attribute Rating`, "and a day after the event the average final rating for the trait was", `Average Final Attribute Rating`,". The graph above shows this trend visually and colors each point depending on how the individuals response changed after a day. Blue represents a positive change, while red represents no/a negitive change in that specific trait. The graph also of trend line that can show the trait change, a positive exponential trend meaning increased ratings and a negitive exponential trend meaning decreased ratings. If the trend appears linear or close to linear that means the trait ratings changed very little if at all. The graph also offers a lot of interactive elements, allowing users to view the data by race, age range, gender, question asked and trait. Users can also click different points on the graph and see the individuals initial rating for that trait and their final rating as well." )
+    return(text.out)
   })
   #master server code ends here--------------------  
 }
